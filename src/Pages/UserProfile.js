3031a489d37de1b7e89778c433ec97b60cfd9301
Cory Pease <coryrpease@gmail.com>
2020-01-12 21:15:54 -0700

    interated over bill component to dynamically render bill cards

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -11,31 +11,66 @@ export class UserProfile extends Component {
         id: uuid.v4(),
         billName: 'Sprint',
         billAmount: 120.35,
-        dueDate: new Date()
-      }
+        dueDate: '25'
+      },      {
+        id: uuid.v4(),
+        billName: 'Sprint',
+        billAmount: 120.35,
+        dueDate: '02'
+      }, {
+        id: uuid.v4(),
+        billName: 'Mortgage',
+        billAmount: 1137,
+        dueDate: '29'
+      }, {
+        id: uuid.v4(),
+        billName: 'Insurance',
+        billAmount: 56.92,
+        dueDate: '31'
+      }, {
+        id: uuid.v4(),
+        billName: 'American Express',
+        billAmount: 194.59,
+        dueDate: '15'
+      },
     ]
   }
+
+  currentDay = new Date().getDate();
+  
   render() {
+    const bills = this.state.bills
+    console.log('currentDay', this.currentDay)
     return (
-      <div>
-        <div style={userStyle}>
+      <div >
+        <div style={topStyle}>
           <img src={profilePicture} alt="User Profile" height='250px' width='250px'/>
           <h1>Cory Pease</h1>
           <AddBill/>
         </div>
+        <div style={billWrapper}>
         {
-          this.state.bills.map(bill => <Bill props={bill}/>)
+          bills
+          .sort(function (a, b) {
+            return a.dueDate - b.dueDate;
+          })
+          .map(bill => <Bill key={bill.id} bill={bill}/>)
         }
+        </div>
       </div>
     )
   }
 }
 
-const userStyle = {
+const topStyle = {
   display: 'flex',
   justifyContent: 'space-around',
   width: '100vw',
   padding: '12px'
+}
+
+const billWrapper = {
+  display: 'flex'
 }
 
 export default UserProfile
--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,58 +1,65 @@
-import React from "react";
+import React, {Component} from "react";
 
-function Bill() {
-  return (
-    <form style={formStyle} action="" method="post">
-      <input
-        style={inputStyle}
-        type="text"
-        name="billName"
-        placeholder="Bill Name"
-      />
-      <input
-        style={inputStyle}
-        type="text"
-        name="billAmount"
-        placeholder="Bill Amount"
-      />
-      <input 
-      style={inputStyle} 
-      type="date" 
-      name="billDate" />
-      <div>
-        <button
-          type="submit"
-          style={{
-            width: "56px",
-            boxShadow:
-              "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-          }}
-        >
-          Edit
-        </button>
-        <button
-          type="submit"
-          style={{
-            width: "56px",
-            boxShadow:
-              "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-          }}
-        >
-          Save
-        </button>
-        <button
-          type="submit"
-          style={{
-            width: "56px",
-            boxShadow:
-              "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    </form>
+export class Bill extends Component {
+  render() {
+    const {billName, billAmount, dueDate} = this.props.bill
+    return (
+      <form style={formStyle} action="" method="post">
+        <input
+          value={billName}
+          style={inputStyle}
+          type="text"
+          name="billName"
+          placeholder="Bill Name"
+        />
+        <input
+          value={billAmount}
+          style={inputStyle}
+          type="text"
+          name="billAmount"
+          placeholder="Bill Amount"
+        />
+        <input 
+        value={dueDate}
+        style={inputStyle} 
+        type="number" 
+        name="billDate" />
+        <div>
+          <button
+            type="submit"
+            style={{
+              width: "56px",
+              boxShadow:
+                "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+            }}
+          >
+            Edit
+          </button>
+          <button
+            type="submit"
+            style={{
+              width: "56px",
+              boxShadow:
+                "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+            }}
+          >
+            Save
+          </button>
+          <button
+            type="submit"
+            style={{
+              width: "56px",
+              boxShadow:
+                "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </form>
   );
+  }
+  
 }
 
 const formStyle = {
@@ -71,7 +78,8 @@ const inputStyle = {
   height: "32px",
   width: "120px",
   marginLeft: "4px",
-  marginRight: "4px"
+  marginRight: "4px",
+  textAlign: 'center'
 };
 
 export default Bill;
