f8d13bb906dad5800950cefcc4f94af8811dec2c
Cory Pease <coryrpease@gmail.com>
2020-01-17 23:06:32 -0700

    clean up

--- src/components/AddBill.js
+++ src/components/AddBill.js
@@ -34,13 +34,16 @@ export class AddBill extends Component {
 
     render() {
         return (
-        <form style={formStyle} onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} value={this.state.billName} style={inputStyle} type="text" name='billName' placeholder='Bill Name'/>
-          <input onChange={this.handleChange} value={this.state.billAmount} style={inputStyle}  type="text" name='billAmount' placeholder='Bill Amount'/>
-          <input onChange={this.handleChange} value={this.state.billDate} style={inputStyle} type="date" name="billDate"/>
-          <button type="submit" style={{  width: '56px', boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)'}}>Add Bill</button>
-        </form>
-      )
+            <form style={formStyle} onSubmit={this.handleSubmit}>
+                <label htmlFor="billName">Bill Name:</label>
+                <input onChange={this.handleChange} value={this.state.billName} style={inputStyle} type="text" name='billName' placeholder='Bill Name'/>
+                <label htmlFor="billAmount">Bill Amount:</label>
+                <input onChange={this.handleChange} value={this.state.billAmount} style={inputStyle}  type="text" name='billAmount' placeholder='Bill Amount'/>
+                <label htmlFor="billDate">Bill Date:</label>
+                <input onChange={this.handleChange} value={this.state.billDate} style={inputStyle} type="date" name="billDate"/>
+                <button type="submit" style={{  width: '56px', boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)'}}>Add Bill</button>
+            </form>
+        )
     }
 }
 
@@ -48,20 +51,20 @@ const addBill = (type = 'ADD_BILL', bill) => {
     return {type, newBill: bill}
 }
 const formStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  height: '25vh',
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
-  border: '1px solid black'
+    display: 'flex',
+    flexDirection: 'column',
+    height: '25vh',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    border: '1px solid black'
 }
 
 const inputStyle = {
-  border: '1px solid black',
-  minHeight: '32px',
-  minWidth: '224px',
-  marginLeft: '4px',
-  marginRight: '4px'
+    border: '1px solid black',
+    minHeight: '32px',
+    minWidth: '224px',
+    marginLeft: '4px',
+    marginRight: '4px'
 }
 
 export default connect(
